e0e4a993bed5a5d41a52dbb3d8ce1c583c7ecb94
syahmisolihin <122283228+syahmisolihin@users.noreply.github.com>
2025-11-24 19:55:18 +0800

    Filtering feature
    dah letak and functional

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class CustomerController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-    $data_customer = \App\Models\Customer::all();
-    return view('index',compact('data_customer'));
-}
+        // Start query
+        $query = Customer::query();
+
+        // Filter by gender
+        if ($request->gender) {
+            $query->where('gender', $request->gender);
+        }
+
+        // Filter by birthday (born after 2000)
+        if ($request->born_after_2000) {
+            $query->whereYear('birthday', '>', 2000);
+        }
+
+        // Get all filtered/unfiltered results
+        $data_customer = $query->get();
+
+        return view('index', compact('data_customer'));
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,24 @@
 <div class='container mt-4'>
     <h1 class='mb-3'>Customer's Data</h1>
 
+    <form action="/" method="GET">
+    <label>Gender:</label>
+    <select name="gender">
+        <option value="">All</option>
+        <option value="M" {{ request('gender') == 'M' ? 'selected' : '' }}>Male</option>
+        <option value="F" {{ request('gender') == 'F' ? 'selected' : '' }}>Female</option>
+    </select>
+
+    <label>
+        <input type="checkbox" name="born_after_2000" value="1"
+        {{ request('born_after_2000') ? 'checked' : '' }}>
+        Born after 2000
+    </label>
+
+    <button type="submit">Filter</button>
+</form>
+
+
     <table class='table table-bordered table-striped'>
         <thead>
             <tr>
