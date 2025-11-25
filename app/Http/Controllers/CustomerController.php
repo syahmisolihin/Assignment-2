84a9d710e4ae4b00da2bfa40b879657b3d838f01
syahmisolihin <122283228+syahmisolihin@users.noreply.github.com>
2025-11-26 01:10:01 +0800

    comment

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -9,20 +9,20 @@ class CustomerController extends Controller
 {
     public function index(Request $request)
     {
-        // Start query
+       
         $query = Customer::query();
 
-        // Filter by gender
+       
         if ($request->gender) {
             $query->where('gender', $request->gender);
         }
 
-        // Filter by birthday (born after 2000)
+        
         if ($request->born_after_2000) {
             $query->whereYear('birthday', '>', 2000);
         }
 
-        // Get all filtered/unfiltered results
+       
         $data_customer = $query->get();
 
         return view('index', compact('data_customer'));
